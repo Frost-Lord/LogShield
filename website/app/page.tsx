c456207ba05174e2dc90cd897707fe8f13c30770
Frost Lord <51778028+Frost-Lord@users.noreply.github.com>
2023-05-17 14:36:32 +1000

    Page optimization's

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,9 +1,7 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
@@ -23,8 +21,6 @@ export default function Home() {
       <AboutSectionTwo />
       <Testimonials />
       <Pricing />
-      <Blog />
-      <Contact />
     </>
   );
 }
